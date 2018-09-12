7c3bcffebf696fa6bec6e2680f47622c3f8df98b
Carlos Ramos <carakan@gmail.com>
2018-09-12 11:13:08 -0400

    add all ecto 2.2 methods to delegate
    also added support for pagination

--- lib/soft_repo.ex
+++ lib/soft_repo.ex
@@ -114,45 +114,32 @@ defmodule SoftRepo do
 
   defp with_force_option?(opts), do: Keyword.get(opts, :force)
 
+  defdelegate aggregate(queryable, aggregate, field, opts), to: @repo
   defdelegate config(), to: @repo
-
-  defdelegate get!(queryable, id, opts \\ []), to: @repo
-
-  defdelegate get_by(queryable, clauses, opts \\ []), to: @repo
-
-  defdelegate get_by!(queryable, clauses, opts \\ []), to: @repo
-
-  defdelegate in_transaction?(), to: @repo
-
-  defdelegate insert(struct, opts \\ []), to: @repo
-
-  defdelegate insert!(struct, opts \\ []), to: @repo
-
-  defdelegate insert_all(schema_or_source, entries, opts \\ []), to: @repo
-
-  defdelegate insert_or_update(changeset, opts \\ []), to: @repo
-
-  defdelegate insert_or_update!(changeset, opts \\ []), to: @repo
-
-  defdelegate one(queryable, opts \\ []), to: @repo
-
-  defdelegate one!(queryable, opts \\ []), to: @repo
-
-  defdelegate preload(struct_or_structs, preloads, opts \\ []), to: @repo
-
-  defdelegate rollback(value), to: @repo
-
-  defdelegate start_link(opts \\ []), to: @repo
-
-  defdelegate stop(pid, timeout \\ 5000), to: @repo
-
-  defdelegate transaction(fun_or_multi, opts \\ []), to: @repo
-
-  defdelegate update(struct, opts \\ []), to: @repo
-
-  defdelegate update!(struct, opts \\ []), to: @repo
-
-  defdelegate update_all(queryable, updates, opts \\ []), to: @repo
-
   defdelegate delete!(struct, opts \\ []), to: @repo
+  defdelegate get!(queryable, id, opts \\ []), to: @repo
+  defdelegate get_by!(queryable, clauses, opts \\ []), to: @repo
+  defdelegate get_by(queryable, clauses, opts \\ []), to: @repo
+  defdelegate in_transaction?(), to: @repo
+  defdelegate init(arg0, config), to: @repo
+  defdelegate insert!(struct, opts \\ []), to: @repo
+  defdelegate insert(struct, opts \\ []), to: @repo
+  defdelegate insert_all(schema_or_source, entries, opts \\ []), to: @repo
+  defdelegate insert_or_update!(changeset, opts \\ []), to: @repo
+  defdelegate insert_or_update(changeset, opts \\ []), to: @repo
+  defdelegate one!(queryable, opts \\ []), to: @repo
+  defdelegate one(queryable, opts \\ []), to: @repo
+  @doc """
+  Scrivener pagination.
+  """
+  defdelegate paginate(pageable, options \\ []), to: @repo
+  defdelegate preload(structs_or_struct_or_nil, preloads, opts \\ []), to: @repo
+  defdelegate rollback(value), to: @repo
+  defdelegate start_link(opts \\ []), to: @repo
+  defdelegate stop(pid, timeout \\ 5000), to: @repo
+  defdelegate stream(queryable, opts), to: @repo
+  defdelegate transaction(fun_or_multi, opts \\ []), to: @repo
+  defdelegate update!(struct, opts \\ []), to: @repo
+  defdelegate update(struct, opts \\ []), to: @repo
+  defdelegate update_all(queryable, updates, opts \\ []), to: @repo
 end
