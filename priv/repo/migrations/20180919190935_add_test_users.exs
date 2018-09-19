19090ffe60787bb958960188b19667be3e9bd385
Carlos Ramos <carakan@gmail.com>
2018-09-19 19:33:10 -0400

    format file

--- priv/repo/migrations/20180919190935_add_test_users.exs
+++ priv/repo/migrations/20180919190935_add_test_users.exs
@@ -3,13 +3,13 @@ defmodule Repo.Migrations.AddTestUsers do
 
   def change do
     create table(:users) do
-      add :token, :string, null: false
-      add :username, :string, null: false
+      add(:token, :string, null: false)
+      add(:username, :string, null: false)
       SoftRepo.Migration.soft_repo_column()
 
       timestamps()
     end
 
-    create index(:users, [:token])
+    create(index(:users, [:token]))
   end
 end
